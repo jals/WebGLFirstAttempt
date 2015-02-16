f5295e0fe734a3c0821781e46a5e552b2414ab58
Jals <astroman_999@hotmail.com>
2015-02-16 08:05:16 -0500

    Added sub-menu items it that show/hide when left/right arrow is pressed. Missing animations at the moment.

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ function initGL(canvas) {
 }
 
 var menuUp = false;
+var selected = 3;
+
 function keyUp(e){
   //  console.log(e.keyCode);
     if(e.keyCode == 77){
@@ -28,11 +30,15 @@ function keyUp(e){
             fadeOut(7);
         }
     } else if(e.keyCode == 37){
-        console.log('left');
+        if(selected > 0){
+            selected -=1 ;
+        }
     } else if(e.keyCode == 38){
         console.log('up');
     } else if(e.keyCode == 39){
-        console.log('right');
+        if(selected < 6){
+            selected += 1;
+        }
     } else if(e.keyCode == 40){
         console.log('down');
     }
@@ -128,7 +134,24 @@ var textureTV;
 var textureRecordings;
 var textureTVShows;
 var textureSearch;
-var textureBlank;
+var subTextureApps1;
+var subTextureApps2;
+var subTextureApps3;
+var subTextureApps4;
+var subTextureHome1;
+var subTextureHome2;
+var subTextureHome3;
+var subTextureHome4;
+var subTextureTV1;
+var subTextureTV2;
+var subTextureRecordings1;
+var subTextureRecordings2;
+var subTextureTVShows1;
+var subTextureTVShows2;
+var subTextureSearch1;
+var subTextureSearch2;
+var subTextureSearch3;
+var subTextureSearch4;
 
 function initTextures() {
     textureSettings = getTexture("SETTINGS", "img/settings.png");
@@ -138,7 +161,21 @@ function initTextures() {
     textureRecordings = getTexture("RECORDINGS", "img/recorded.png");
     textureTVShows = getTexture("TV SHOWS", "img/on_demand.png");
     textureSearch = getTexture("SEARCH", "img/search.png");
-    textureBlank = getTexture("TEST", "");
+   
+    subTextureHome1 = getText("FAMILY");
+    subTextureHome2 = getText("ANDREA");
+    subTextureHome3 = getText("COOPER");
+    subTextureHome4 = getText("OPTIONS");
+    subTextureTV1 = getText("GUIDE");
+    subTextureTV2 = getText("WHAT'S ON");
+    subTextureRecordings1 = getText("SETUP");
+    subTextureRecordings2 = getText("RECENT");
+    subTextureTVShows1 = getText("POPULAR");
+    subTextureTVShows2 = getText("FAVOURITES");
+    subTextureSearch1 = getText("DISTRICT B13");
+    subTextureSearch2 = getText("PARKOUR");
+    subTextureSearch3 = getText("NETFLIX");
+    subTextureSearch4 = getText("CLEAR RECENT");
 }
 
 function getTexture(text, imagePath){
@@ -151,17 +188,39 @@ function getTexture(text, imagePath){
     ctx.fillStyle = 'white';
     ctx.font = "35px calibri";
     ctx.textAlign = 'center';            
-    ctx.fillText(text, 200 / 2, ctx.canvas.height-10);
+    ctx.fillText(text, ctx.canvas.width/2, ctx.canvas.height-10);
 
     var texture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([77, 77, 77, 255]));
+    
     texture.image = new Image();
     img.onload = function () {
-        ctx.drawImage(img, -17, -4, 200/2-img.width/2, 130/2-img.height/2-5, 0, 0, 200, 100);
+        ctx.drawImage(img, -17, -4, ctx.canvas.width/2-img.width/2, ctx.canvas.height/2-img.height/2-5, 0, 0, 200, 100);
         handleTextureLoaded(canvas, texture);
     }    
     img.src = imagePath;
+
+    return texture;
+}
+
+function getText(text){
+    var canvas = document.createElement('canvas');
+    var ctx = canvas.getContext('2d');
+    canvas.width  = 200;
+    canvas.height = 50;
+    var img = new Image();
+
+    ctx.fillStyle = 'white';
+    ctx.font = "30px calibri";
+    ctx.textAlign = 'center';            
+    ctx.fillText(text, ctx.canvas.width / 2, ctx.canvas.height-14);
+
+    var texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([77, 77, 77, 255]));
+    
+    handleTextureLoaded(canvas, texture);
     return texture;
 }
 
@@ -258,28 +317,50 @@ function drawScene() {
     gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    //mat4.perspective(90, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
     mat4.ortho(-8, 10, -8, 6, 0.1, 100, pMatrix);
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, [0.0, moveOutOffset1, -8.0]);
 
     var diff = 2.1;
-    addButton(-3*diff, moveOutOffset4, textureSettings);
-    addButton(-2*diff, moveOutOffset3, textureApps);
-    addButton(-1*diff, moveOutOffset2, textureHome);
-    addButton(0, moveOutOffset1, textureTV);
-    addButton(diff, moveOutOffset2, textureRecordings);
-    addButton(2*diff, moveOutOffset3, textureTVShows);
-    addButton(3*diff, moveOutOffset4, textureSearch);
-    addButton(0, moveOutOffset1, textureBlank);
+    var diff2 = 0.91
+    addButton(-3*diff, 0, moveOutOffset4, textureSettings, false);
+    addButton(-2*diff, 0, moveOutOffset3, textureApps, false);
+    addButton(-1*diff, 0, moveOutOffset2, textureHome, false);
+    addButton(0, 0, moveOutOffset1, textureTV, false);
+    addButton(diff, 0, moveOutOffset2, textureRecordings, false);
+    addButton(2*diff, 0, moveOutOffset3, textureTVShows, false);
+    addButton(3*diff, 0, moveOutOffset4, textureSearch, false);
+
+    if(selected == 2){
+        addButton(-1*diff, diff2, moveOutOffset1, subTextureHome1, true);
+        addButton(-1*diff, 2*diff2, moveOutOffset1, subTextureHome2, true);
+        addButton(-1*diff, 3*diff2, moveOutOffset1, subTextureHome3, true);
+        addButton(-1*diff, 4*diff2, moveOutOffset1, subTextureHome4, true);
+    } else if(selected == 3){
+        addButton(0, diff2, moveOutOffset1, subTextureTV1, true);
+        addButton(0, 2*diff2, moveOutOffset1, subTextureTV2, true);
+    } else if(selected == 4){
+        addButton(diff, diff2, moveOutOffset1, subTextureRecordings1, true);
+        addButton(diff, 2*diff2, moveOutOffset1, subTextureRecordings2, true);
+    } else if(selected == 5){
+        addButton(2*diff, diff2, moveOutOffset1, subTextureTVShows1, true);
+        addButton(2*diff, 2*diff2, moveOutOffset1, subTextureTVShows2, true);
+    } else if(selected == 6){
+        addButton(3*diff, diff2, moveOutOffset1, subTextureSearch1, true);
+        addButton(3*diff, 2*diff2, moveOutOffset1, subTextureSearch2, true);
+        addButton(3*diff, 3*diff2, moveOutOffset1, subTextureSearch3, true);
+        addButton(3*diff, 4*diff2, moveOutOffset1, subTextureSearch4, true);
+    }
 }
 
-function addButton(offset, moveOutOffset, texture){
+function addButton(offset, heightOffset, moveOutOffset, texture, isSmaller){
     mat4.identity(mvMatrix);
     mat4.translate(mvMatrix, [0.0, moveOutOffset, -8.0]);
 
-    mat4.translate(mvMatrix, [offset, 0.0, 0.0]);
-    //mat4.scale(mvMatrix, [1.3, 1.1, 0.1]);
+    mat4.translate(mvMatrix, [offset, heightOffset, 0.0]);
+    if(isSmaller){
+        mat4.scale(mvMatrix, [1.0, 0.4, 1.0]);
+    }
     gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 
